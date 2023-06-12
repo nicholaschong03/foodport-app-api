4cab5e126eed035902caa7f1302766348bc03168
Nicholas Chong <chongyy03@gmail.com>
2023-06-12 22:42:12 +0800

    updated serializer method field to return postPhoto url

--- app/menu/serializers.py
+++ app/menu/serializers.py
@@ -7,6 +7,9 @@ from decimal import Decimal, InvalidOperation
 
 from core.models import MenuItem, Post
 
+from django.conf import settings
+import os
+
 class MenuItemSerializer(serializers.ModelSerializer):
     """Serializer for menu item """
     id = serializers.ReadOnlyField()
@@ -23,7 +26,9 @@ class MenuItemSerializer(serializers.ModelSerializer):
     def get_post_photo_url(self, obj):
        post = Post.objects.filter(menuItemId=obj.id).order_by("-postLikeCount").first()
        if post and post.postPhotoUrl:
-           return post.postPhotoUrl
+           request = self.context.get("request")
+           photo_url = post.postPhotoUrl.url
+           return request.build_absolute_uri(photo_url)
        return None
 
 
@@ -47,8 +52,8 @@ class MenuItemDetailSerializer(MenuItemSerializer):
                   "post_photos_url"]
 
     def get_post_photos_url(self, obj):
+        request = self.context.get("request")
         posts = Post.objects.filter(menuItemId=obj.id).order_by("-postLikeCount")
-        return {post.id: post.postPhotoUrl for post in posts if post.postPhotoUrl}
-
+        return {post.id: request.build_absolute_uri(post.postPhotoUrl.url) for post in posts if post.postPhotoUrl}
 
 
