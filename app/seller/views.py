6b7f5f34ec2569b04bf345637777d1602bfd6f0b
Nicholas Chong <chongyy03@gmail.com>
2023-07-08 13:14:26 +0800

    added logic to analyze-performance api for different timeframe

--- app/seller/views.py
+++ app/seller/views.py
@@ -15,8 +15,6 @@ from datetime import datetime, timedelta
 from django.http import Http404
 
 
-
-
 class SellerViewset(viewsets.ModelViewSet):
     """View for manage seller APIs"""
     serializer_class = SellerDetailSerializer
@@ -45,13 +43,15 @@ class SellerViewset(viewsets.ModelViewSet):
                 "IP_address": ip_address,
             }
         }
-        serializer.save(user=user, sellerInfoContributor=seller_info_contributor)
+        serializer.save(
+            user=user, sellerInfoContributor=seller_info_contributor)
 
 
 class CustomSellerPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 100
+
 
 class SellerListView(generics.ListAPIView):
     queryset = Seller.objects.all().order_by("sellerBusinessName")
@@ -61,6 +61,7 @@ class SellerListView(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ["sellerBusinessName"]
     pagination_class = CustomSellerPagination
+
 
 class RetrieveSellerView(generics.RetrieveAPIView):
     """Retrieve a seller"""
@@ -75,6 +76,7 @@ class RetrieveSellerView(generics.RetrieveAPIView):
         except Seller.DoesNotExist:
             raise Http404("Seller not found")
         return seller
+
 
 class AllSellersListView(generics.ListAPIView):
     """Return a list of all existing menu items"""
@@ -103,16 +105,20 @@ class LikePercentageChangeView(APIView):
         if not startDateTime or not endDateTime:
             return Response({"error": "startDateTime and endDateTime query params are required"}, status=400)
 
-        startDateTime = startDateTime.replace("_", " ").replace("%20", " ").replace("%22", " ").replace("%3A", ":").strip('"').strip()
-        endDateTime = endDateTime.replace("_", " ").replace("%20", " ").replace("%22", " ").replace("%3A", ":").strip('"').strip()
+        startDateTime = startDateTime.replace("_", " ").replace(
+            "%20", " ").replace("%22", " ").replace("%3A", ":").strip('"').strip()
+        endDateTime = endDateTime.replace("_", " ").replace("%20", " ").replace(
+            "%22", " ").replace("%3A", ":").strip('"').strip()
 
         print('Start Date:', startDateTime)  # Debugging print statement
         print('End Date:', endDateTime)  # Debugging print statement
 
         # Parse the datetime strings into datetime objects
         try:
-            startDateTime = datetime.strptime(startDateTime, "%Y-%m-%dT%H:%M:%S.%fZ")
-            endDateTime = datetime.strptime(endDateTime, "%Y-%m-%dT%H:%M:%S.%fZ")
+            startDateTime = datetime.strptime(
+                startDateTime, "%Y-%m-%dT%H:%M:%S.%fZ")
+            endDateTime = datetime.strptime(
+                endDateTime, "%Y-%m-%dT%H:%M:%S.%fZ")
         except ValueError:
             return Response({"error": "Invalid datetime format. Use ISO 8601 format 'YYYY-MM-DDTHH:MM:SS.sssZ'",
                              "startDate": startDateTime,
@@ -125,8 +131,10 @@ class LikePercentageChangeView(APIView):
         posts = Post.objects.filter(menuItemId__in=menu_items)
 
         # Calcualte likes at startDateTime and endDateTie
-        start_likes = posts.filter(postPublishDateTime__lte=startDateTime).aggregate(sum=Sum("postLikeCount"))["sum"] or 0
-        end_likes = posts.filter(postPublishDateTime__lte=endDateTime).aggregate(sum=Sum("postLikeCount"))["sum"] or 0
+        start_likes = posts.filter(postPublishDateTime__lte=startDateTime).aggregate(
+            sum=Sum("postLikeCount"))["sum"] or 0
+        end_likes = posts.filter(postPublishDateTime__lte=endDateTime).aggregate(
+            sum=Sum("postLikeCount"))["sum"] or 0
 
         # calculate trend direction and percentage change
         if start_likes == end_likes:
@@ -144,14 +152,12 @@ class LikePercentageChangeView(APIView):
                          "trend": trend,
                          "trendPercentage": percentage_change})
 
+
 class DailyCumulativePostLikesView(APIView):
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
-    def get(self,request, sellerId,startDateTime, endDateTime, *args, **kwargs):
-        # sellerId = self.request.query_params.get("sellerId")
-        # startDateTime = self.request.query_params.get("startDateTime")
-        # endDateTime = self.request.query_params.get("endDateTime")
+    def get(self, request, sellerId, startDateTime, endDateTime, *args, **kwargs):
 
         try:
             seller = Seller.objects.get(id=sellerId)
@@ -162,18 +168,21 @@ class DailyCumulativePostLikesView(APIView):
         if not startDateTime or not endDateTime:
             return Response({"error": "startDateTime and endDateTime query params are required"}, status=400)
 
-        startDateTime = startDateTime.replace("_", " ").replace("%20", " ").replace("%22", " ").replace("%3A", ":").strip('"').strip()
-        endDateTime = endDateTime.replace("_", " ").replace("%20", " ").replace("%22", " ").replace("%3A", ":").strip('"').strip()
+        startDateTime = startDateTime.replace("_", " ").replace(
+            "%20", " ").replace("%22", " ").replace("%3A", ":").strip('"').strip()
+        endDateTime = endDateTime.replace("_", " ").replace("%20", " ").replace(
+            "%22", " ").replace("%3A", ":").strip('"').strip()
 
         # Parse the datetime strings into datetime objects
         try:
-            startDateTime = datetime.strptime(startDateTime, "%Y-%m-%dT%H:%M:%S.%fZ")
-            endDateTime = datetime.strptime(endDateTime, "%Y-%m-%dT%H:%M:%S.%fZ")
+            startDateTime = datetime.strptime(
+                startDateTime, "%Y-%m-%dT%H:%M:%S.%fZ")
+            endDateTime = datetime.strptime(
+                endDateTime, "%Y-%m-%dT%H:%M:%S.%fZ")
         except ValueError:
             return Response({"error": "Invalid datetime format. Use ISO 8601 format 'YYYY-MM-DDTHH:MM:SS.sssZ'",
                              "startDate": startDateTime,
                              "sellerId": sellerId}, status=400,)
-
 
         # Get the seller's menu items
         menu_items = MenuItem.objects.filter(id__in=seller.menuItemId)
@@ -184,20 +193,35 @@ class DailyCumulativePostLikesView(APIView):
         # Initialize the result list
         results = []
 
-        # For each day in the range
+        delta = endDateTime - startDateTime
+        if delta <= timedelta(hours=24):
+            # If range is 24 hours, step is 2 hours
+            step = timedelta(hours=2)
+        elif delta <= timedelta(days=7):
+            # If range is a week, step is one day
+            step = timedelta(days=1)
+        elif delta <= timedelta(days=31):
+            # If range is a month, step is a week
+            step = timedelta(days=7)
+        elif delta <= timedelta(days=90):
+            # If range is a quarter, step is one month
+            step = timedelta(days=30)
+        elif delta <= timedelta(days=365):
+            # If range is a year, step is one month
+            step = timedelta(days=30)
+        else:
+            # Default step is one day
+            step = timedelta(days=1)
+
         day = startDateTime
         while day <= endDateTime:
             # Get the cumulative sum of likes for posts up to and including this day
-            likes = posts.filter(postPublishDateTime__lte=day).aggregate(sum=Sum("postLikeCount"))["sum"] or 0
+            likes = posts.filter(postPublishDateTime__lte=day).aggregate(
+                sum=Sum("postLikeCount"))["sum"] or 0
             # Append the result to the results list
             results.append({"timestamp": day.isoformat(), "like": likes})
-            # Move to the next day
-            day += timedelta(days=1)
+            # Move to the next step
+            day += step
 
         # Return the result
         return Response({"totalPostLike": results})
-
-
-
-
-
