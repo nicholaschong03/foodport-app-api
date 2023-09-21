0e68632e9e48d308988c1cde919b802bede19b4e
Nicholas Chong <chongyy03@gmail.com>
2023-09-21 17:28:29 +0800

    optimized user profile picture, and post photo

--- app/core/models.py
+++ app/core/models.py
@@ -21,6 +21,13 @@ from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFill
 from imagekit.processors import Adjust
 
+from PIL import Image
+from django.core.files.base import ContentFile
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from io import BytesIO
+
+import sys
+
 
 def post_image_file_path(instance, filename):
     """Genrate file path for new post image."""
@@ -106,6 +113,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     userBio = models.TextField(blank=True)
     userProfilePictureUrl = models.ImageField(
         null=True, upload_to=user_image_file_path)
+
     userBirthDate = models.DateField(blank=True, null=True)
     userAccountRegisterDate = models.DateTimeField(auto_now_add=True)
     userPostLike = models.JSONField(default=dict, blank=True)
@@ -129,8 +137,112 @@ class User(AbstractBaseUser, PermissionsMixin):
     userCoverPictureUrl = models.ImageField(
         null=True, upload_to=user_cover_picture_file_path)
     IPv4 = models.JSONField(default=dict, null=True, blank=True)
-    userLatitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
-    userLongitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    userLatitude = models.DecimalField(
+        max_digits=9, decimal_places=6, null=True, blank=True)
+    userLongitude = models.DecimalField(
+        max_digits=9, decimal_places=6, null=True, blank=True)
+
+    # def save(self, *args, **kwargs):
+    #     # Storing the old file paths before calling super().save()
+    #     if self.pk:
+    #         old_instance = User.objects.get(pk=self.pk)
+    #         old_profile_pic = old_instance.userProfilePictureUrl.path if old_instance.userProfilePictureUrl else None
+    #         old_cover_pic = old_instance.userCoverPictureUrl.path if old_instance.userCoverPictureUrl else None
+    #     else:
+    #         old_profile_pic = None
+    #         old_cover_pic = None
+
+    #     super().save(*args, **kwargs)
+
+    #     # Handle UserProfilePictureUrl
+    #     if self.userProfilePictureUrl and hasattr(self.userProfilePictureUrl, 'file'):
+    #         # Deleting the old file if it exists
+    #         if old_profile_pic and os.path.exists(old_profile_pic):
+    #             os.remove(old_profile_pic)
+
+    #         # Your existing image handling code
+    #         img = Image.open(self.userProfilePictureUrl)
+    #         img = img.convert('RGB')
+    #         img = img.resize((800, 800), Image.ANTIALIAS)
+    #         buffer = BytesIO()
+    #         img.save(buffer, format='JPEG', quality=85)
+    #         buffer.seek(0)
+    #         self.userProfilePictureUrl.save(name=self.userProfilePictureUrl.name, content=ContentFile(buffer.read()), save=False)
+
+    #     # Handle UserCoverPictureUrl
+    #     if self.userCoverPictureUrl and hasattr(self.userCoverPictureUrl, 'file'):
+    #         # Deleting the old file if it exists
+    #         if old_cover_pic and os.path.exists(old_cover_pic):
+    #             os.remove(old_cover_pic)
+
+    #         # Your existing image handling code
+    #         img = Image.open(self.userCoverPictureUrl)
+    #         img = img.convert('RGB')
+    #         img = img.resize((800, 800), Image.ANTIALIAS)
+    #         buffer = BytesIO()
+    #         img.save(buffer, format='JPEG', quality=85)
+    #         buffer.seek(0)
+    #         self.userCoverPictureUrl.save(name=self.userCoverPictureUrl.name, content=ContentFile(buffer.read()), save=False)
+
+    #     super().save(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        # Store the old image paths before saving the new images
+        if self.pk:
+            old_instance = User.objects.get(pk=self.pk)
+            old_profile_pic_path = old_instance.userProfilePictureUrl.path if old_instance.userProfilePictureUrl else None
+            old_cover_pic_path = old_instance.userCoverPictureUrl.path if old_instance.userCoverPictureUrl else None
+        else:
+            old_profile_pic_path = None
+            old_cover_pic_path = None
+
+        profile_pic_modified = False
+        cover_pic_modified = False
+        super(User, self).save(*args, **kwargs)
+
+        if self.userProfilePictureUrl and hasattr(self.userProfilePictureUrl, 'file'):
+            # Open the image using Pillow
+            img = Image.open(self.userProfilePictureUrl)
+
+            # Resize the image.
+            img = img.resize((1290, 1290), Image.ANTIALIAS)
+
+            # Save the image back to userProfilePictureUrl
+            buffer = BytesIO()
+            img.save(fp=buffer, format="JPEG", quality=85)
+            buffer.seek(0)
+            self.userProfilePictureUrl.save(
+                name=self.userProfilePictureUrl.name, content=ContentFile(buffer.read()), save=False)
+
+            profile_pic_modified = True
+
+        if self.userCoverPictureUrl and hasattr(self.userCoverPictureUrl, 'file'):
+            # Open the image using Pillow
+            img = Image.open(self.userCoverPictureUrl)
+
+            # Resize the image.
+            img = img.resize((1290, 1290), Image.ANTIALIAS)
+
+            # Save the image back to userCoverPictureUrl
+            buffer = BytesIO()
+            img.save(fp=buffer, format="JPEG", quality=85)
+            buffer.seek(0)
+            self.userCoverPictureUrl.save(name=self.userCoverPictureUrl.name, content=ContentFile(
+                buffer.read()), save=False)  # Corrected this line
+
+            cover_pic_modified = True
+
+        if profile_pic_modified or cover_pic_modified:
+            # Call the "real" save() method once if any image field was modified
+            super(User, self).save(*args, **kwargs)
+
+        # Delete the old image files from the filesystem
+        if old_profile_pic_path and profile_pic_modified:
+            if os.path.exists(old_profile_pic_path):
+                os.remove(old_profile_pic_path)
+        if old_cover_pic_path and cover_pic_modified:
+            if os.path.exists(old_cover_pic_path):
+                os.remove(old_cover_pic_path)
 
     def get_friends(self):
         """Return a QuerySet of friends of the user"""
@@ -155,8 +267,10 @@ class Business(models.Model):
     businessHalal = models.BooleanField(default=False)
     sellerId = models.IntegerField(null=True, blank=True)
     businessInfoContributor = models.JSONField(null=True, blank=True)
-    businessOperatingLatitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
-    businessOperatingLongitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    businessOperatingLatitude = models.DecimalField(
+        max_digits=9, decimal_places=6, null=True, blank=True)
+    businessOperatingLongitude = models.DecimalField(
+        max_digits=9, decimal_places=6, null=True, blank=True)
     businessTrendRanking = models.IntegerField(null=True, blank=True)
     followers = models.ManyToManyField(
         settings.AUTH_USER_MODEL, related_name="following_businesses", blank=True
@@ -167,6 +281,7 @@ class Business(models.Model):
 
     # def get_all_menu_item_ids(self):
     #     return self.menuItemId or []
+
 
 class MenuItem(models.Model):
     """Menu object"""
@@ -179,7 +294,8 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     dishInfoContributor = models.JSONField(default=dict, blank=True, null=True)
     sellerId = models.IntegerField(null=True, blank=True)
-    business = models.ForeignKey(Business, on_delete=models.SET_NULL, null=True, blank=True, related_name="menu_items")
+    business = models.ForeignKey(
+        Business, on_delete=models.SET_NULL, null=True, blank=True, related_name="menu_items")
     category = models.CharField(max_length=255)
 
     basicIngredient = models.JSONField(default=list, blank=True, null=True)
@@ -221,11 +337,6 @@ class Post(models.Model):
         error_messages={"blank": "Please provide a rating from 1 to 5"})
     postPublishIpAddress = models.GenericIPAddressField(null=True, blank=True)
     postPhotoUrl = models.ImageField(null=True, upload_to=post_image_file_path)
-    optimized_image = ImageSpecField(source="postPhotoUrl",
-                                     processors=[ResizeToFill(800,800)],
-                                     format="JPEG",
-                                     options={"quality": 85})
-
     postView = models.JSONField(default=dict, blank=True)
     postComment = models.JSONField(default=dict, blank=True)
     postCommentView = models.JSONField(default=dict, blank=True)
@@ -234,7 +345,30 @@ class Post(models.Model):
     postDishSellerVisit = models.JSONField(default=dict, blank=True)
     postDishVisit = models.JSONField(default=dict, blank=True)
     postLikeCount = models.IntegerField(default=0)
-    menuItem = models.ForeignKey(MenuItem, null=True, blank=True, related_name="posts", on_delete=models.SET_NULL)
+    menuItem = models.ForeignKey(
+        MenuItem, null=True, blank=True, related_name="posts", on_delete=models.SET_NULL)
+
+    def save(self, *args, **kwargs):
+        # Call the "real" save() method
+        super(Post, self).save(*args, **kwargs)
+
+        if self.postPhotoUrl and hasattr(self.postPhotoUrl, 'file'):
+
+            # Open the image using Pillow
+            img = Image.open(self.postPhotoUrl)
+
+            # Resize the image.
+            img = img.resize((1290, 1290), Image.ANTIALIAS)
+
+            # Save the image back to postPhotoUrl
+            buffer = BytesIO()
+            img.save(fp=buffer, format="JPEG", quality=85)
+            buffer.seek(0)
+            self.postPhotoUrl.save(
+                name=self.postPhotoUrl.name, content=ContentFile(buffer.read()), save=False)
+
+            # Call the "real" save() method again
+            super(Post, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.postReview
@@ -287,18 +421,23 @@ class PostComment(models.Model):
         Post, on_delete=models.CASCADE, related_name="comments")
     commentContent = models.TextField()
     commentPublishDateTime = models.DateTimeField(auto_now_add=True)
-    commentPublishIpAddress = models.GenericIPAddressField(null=True, blank=True)
+    commentPublishIpAddress = models.GenericIPAddressField(
+        null=True, blank=True)
     commentUserAgent = models.TextField(null=True, blank=True)
     commentLikes = models.JSONField(blank=True, null=True, default=list)
     commentReplies = models.JSONField(blank=True, null=True, default=list)
-    commentPublishLastUpdatedDateTime = models.DateTimeField(null=True, blank=True)
-    commentPublishLocation = models.JSONField(default=dict, null=True, blank=True)
+    commentPublishLastUpdatedDateTime = models.DateTimeField(
+        null=True, blank=True)
+    commentPublishLocation = models.JSONField(
+        default=dict, null=True, blank=True)
     commentLikeCount = models.IntegerField(default=0)
 
 
 class CommentLike(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment_likes")
-    comment = models.ForeignKey(PostComment, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comment_likes")
+    comment = models.ForeignKey(
+        PostComment, on_delete=models.CASCADE, related_name="likes")
     isActive = models.BooleanField(default=True)
     likeDateTime = models.DateTimeField(auto_now_add=True)
     likeIpAddress = models.GenericIPAddressField(null=True, blank=True)
@@ -307,19 +446,20 @@ class CommentLike(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         comment = self.comment
-        like_count = CommentLike.objects.filter(comment=comment, isActive=True).count()
+        like_count = CommentLike.objects.filter(
+            comment=comment, isActive=True).count()
         unlike_count = CommentLike.objects.filter(
             comment=comment, isActive=False).count()
         comment.commentLikeCount = like_count - unlike_count
         comment.save()
 
 
-
-
 class PostShare(models.Model):
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
-    sharedBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name="shared_post")
-    sharedTo = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_post")
+    sharedBy = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="shared_post")
+    sharedTo = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="received_post")
     sharedDateTime = models.DateTimeField(auto_now_add=True)
 
 
@@ -343,9 +483,6 @@ class Seller(models.Model):
         return self.sellerBusinessName
 
 
-
-
-
 class Dish(models.Model):
     """Dish object"""
     dishName = models.CharField(max_length=255)
@@ -365,6 +502,3 @@ class Dish(models.Model):
     sellerId = models.IntegerField(null=True, blank=True)
     postId = models.JSONField(null=True, blank=True, default=list)
     dishInfoContributor = models.JSONField(default=dict, blank=True)
-
-
-
